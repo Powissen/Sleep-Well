27bfc65466391d6a07eaff07785f76655f2a4493
Powissen <lukasloksa2@gmail.com>
2022-03-18 08:12:19 +0100

    Added basic work with time

--- Sleep-well/Sleep-well/MainPage.xaml.cs
+++ Sleep-well/Sleep-well/MainPage.xaml.cs
@@ -13,6 +13,25 @@ namespace Sleep_well
         public MainPage()
         {
             InitializeComponent();
+            //test
+        }
+        void RefreshTime(object sender, EventArgs args)
+        {
+            time.Text = (DateTime.Now.Hour + ":" + DateTime.Now.Minute).ToString();
+
+            int hours;
+            int minutes;
+
+            hours = 23 - DateTime.Now.Hour + 6;
+            minutes = 60 - DateTime.Now.Minute + 0;
+
+            if (minutes > 60)
+            {
+                hours++;
+                minutes -= 60;
+            }
+
+            alarmAfter.Text = "Budík o " + hours + " hodín a " + minutes + " minút";
         }
     }
 }
