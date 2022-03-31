697a6348cf02680b275e830b28be7e34f9af7e9e
Lukáš Lokša <lukasloksa2@gmail.com>
2022-03-31 21:20:20 +0200

    Added settings and edited basic alarm function

--- Sleep-well/Sleep-well/MainPage.xaml.cs
+++ Sleep-well/Sleep-well/MainPage.xaml.cs
@@ -6,19 +6,38 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
-namespace Sleep_well
+namespace SleepWell
 {
     public partial class MainPage : ContentPage
     {
+        bool bud = true;
+        string s = "";
+        int Hoursbudik;
+        int Minutesbudik;
+
         public MainPage()
         {
             InitializeComponent();
-            //test
         }
+
+        void OpenSettings(object sender, EventArgs args)
+        {
+            App.Current.MainPage = new Settings();
+        }
+
+
         void RefreshTime(object sender, EventArgs args)
         {
-            time.Text = (DateTime.Now.Hour + ":" + DateTime.Now.Minute).ToString();
 
+            if (DateTime.Now.Minute < 10)
+            {
+                time.Text = (DateTime.Now.Hour + ":0" + DateTime.Now.Minute).ToString();
+            }
+            else
+            {
+
+                time.Text = (DateTime.Now.Hour + ":" + DateTime.Now.Minute).ToString();
+            }
             int hours;
             int minutes;
 
@@ -30,8 +49,239 @@ namespace Sleep_well
                 hours++;
                 minutes -= 60;
             }
+            s = "nie je budik";
+            if (bud)
+            {
+                s = "nie je budik";
+            }
+            else
+            {
 
-            alarmAfter.Text = "Budík o " + hours + " hodín a " + minutes + " minút";
+                if ((-(DateTime.Now.Minute - Minutesbudik)) < 0)
+                {
+                    if (DateTime.Now.Hour > 15)
+                    {
+                        s = "Budík o " + (24 - DateTime.Now.Hour + Hoursbudik - 1).ToString() + " hodín a " + (60 - (DateTime.Now.Minute - Minutesbudik)).ToString() + " minút.";
+                    }
+                    else
+                    {
+                        s = "Budík o " + (Hoursbudik - DateTime.Now.Hour - 1).ToString() + " hodín a " + (60 - (DateTime.Now.Minute - Minutesbudik)).ToString() + " minút.";
+                    }
+                }
+                else
+                {
+                    if (DateTime.Now.Hour > 15)
+                    {
+                        s = "Budík o " + (24 - DateTime.Now.Hour + Hoursbudik).ToString() + " hodín a " + (-(DateTime.Now.Minute - Minutesbudik)).ToString() + " minút.";
+                    }
+                    else
+                    {
+                        s = "Budík o " + (Hoursbudik - DateTime.Now.Hour).ToString() + " hodín a " + (-(DateTime.Now.Minute - Minutesbudik)).ToString() + " minút.";
+                    }
+                }
+
+
+
+            }
+
+            alarmAfter.Text = s;
+
+            if (Minutesbudik == DateTime.Now.Minute)
+            {
+                if (Hoursbudik == DateTime.Now.Hour)
+                {
+                    alarmAfter.Text = ("vstávaj");
+
+                }
+
+            }
+            if (Minutesbudik + 1 == DateTime.Now.Minute)
+            {
+                if (Hoursbudik == DateTime.Now.Hour)
+                {
+                    alarmAfter.Text = ("nie je budik");
+
+                }
+
+            }
         }
+        void o10(object sender, EventArgs args)
+        {
+            int hours2 = DateTime.Now.Hour;
+            int minutes2 = DateTime.Now.Minute;
+            if (DateTime.Now.Minute < 49)
+            {
+                minutes2 = DateTime.Now.Minute + 10;
+            }
+            else
+            {
+                hours2++;
+                minutes2 = minutes2 - 50;
+            }
+            hours2 = hours2 + 9;
+            if (hours2 > 24)
+            {
+                hours2 = hours2 - 24;
+            }
+
+
+            Hoursbudik = hours2;
+            Minutesbudik = minutes2;
+            bud = false;
+            if (DateTime.Now.Minute < 10)
+            {
+                time.Text = (DateTime.Now.Hour + ":0" + DateTime.Now.Minute).ToString();
+            }
+            else
+            {
+
+                time.Text = (DateTime.Now.Hour + ":" + DateTime.Now.Minute).ToString();
+            }
+            int hours;
+            int minutes;
+
+            hours = 23 - DateTime.Now.Hour + 6;
+            minutes = 60 - DateTime.Now.Minute + 0;
+
+            if (minutes > 60)
+            {
+                hours++;
+                minutes -= 60;
+            }
+            s = "nie je budik";
+            if (bud)
+            {
+                s = "nie je budik";
+            }
+            else
+            {
+
+                if ((-(DateTime.Now.Minute - Minutesbudik)) < 0)
+                {
+                    if (DateTime.Now.Hour > 15)
+                    {
+                        s = "Budík o " + (24 - DateTime.Now.Hour + Hoursbudik - 1).ToString() + " hodín a " + (60 - (DateTime.Now.Minute - Minutesbudik)).ToString() + " minút.";
+                    }
+                    else
+                    {
+                        s = "Budík o " + (Hoursbudik - DateTime.Now.Hour - 1).ToString() + " hodín a " + (60 - (DateTime.Now.Minute - Minutesbudik)).ToString() + " minút.";
+                    }
+                }
+                else
+                {
+                    if (DateTime.Now.Hour > 15)
+                    {
+                        s = "Budík o " + (24 - DateTime.Now.Hour + Hoursbudik).ToString() + " hodín a " + (-(DateTime.Now.Minute - Minutesbudik)).ToString() + " minút.";
+                    }
+                    else
+                    {
+                        s = "Budík o " + (Hoursbudik - DateTime.Now.Hour).ToString() + " hodín a " + (-(DateTime.Now.Minute - Minutesbudik)).ToString() + " minút.";
+                    }
+                }
+
+
+
+            }
+
+            alarmAfter.Text = s;
+
+            if (Minutesbudik == DateTime.Now.Minute)
+            {
+                if (Hoursbudik == DateTime.Now.Hour)
+                {
+                    alarmAfter.Text = ("vstávaj");
+                }
+            }
+
+        }
+        void o0(object sender, EventArgs args)
+        {
+            int hours2 = DateTime.Now.Hour;
+            int minutes2 = DateTime.Now.Minute;
+            if (DateTime.Now.Minute < 49)
+            {
+                minutes2 = DateTime.Now.Minute;
+            }
+            else
+            {
+                hours2++;
+                minutes2 = minutes2 - 60;
+            }
+            hours2 = hours2 + 9;
+            if (hours2 > 24)
+            {
+                hours2 = hours2 - 24;
+            }
+
+
+            Hoursbudik = hours2;
+            Minutesbudik = minutes2;
+            bud = false;
+            if (DateTime.Now.Minute < 10)
+            {
+                time.Text = (DateTime.Now.Hour + ":0" + DateTime.Now.Minute).ToString();
+            }
+            else
+            {
+
+                time.Text = (DateTime.Now.Hour + ":" + DateTime.Now.Minute).ToString();
+            }
+            int hours;
+            int minutes;
+
+            hours = 23 - DateTime.Now.Hour + 6;
+            minutes = 60 - DateTime.Now.Minute + 0;
+
+            if (minutes > 60)
+            {
+                hours++;
+                minutes -= 60;
+            }
+            s = "nie je budik";
+            if (bud)
+            {
+                s = "nie je budik";
+            }
+            else
+            {
+
+                if ((-(DateTime.Now.Minute - Minutesbudik)) < 0)
+                {
+                    if (DateTime.Now.Hour > 15)
+                    {
+                        s = "Budík o " + (24 - DateTime.Now.Hour + Hoursbudik - 1).ToString() + " hodín a " + (60 - (DateTime.Now.Minute - Minutesbudik)).ToString() + " minút.";
+                    }
+                    else
+                    {
+                        s = "Budík o " + (Hoursbudik - DateTime.Now.Hour - 1).ToString() + " hodín a " + (60 - (DateTime.Now.Minute - Minutesbudik)).ToString() + " minút.";
+                    }
+                }
+                else
+                {
+                    if (DateTime.Now.Hour > 15)
+                    {
+                        s = "Budík o " + (24 - DateTime.Now.Hour + Hoursbudik).ToString() + " hodín a " + (-(DateTime.Now.Minute - Minutesbudik)).ToString() + " minút.";
+                    }
+                    else
+                    {
+                        s = "Budík o " + (Hoursbudik - DateTime.Now.Hour).ToString() + " hodín a " + (-(DateTime.Now.Minute - Minutesbudik)).ToString() + " minút.";
+                    }
+                }
+
+
+
+            }
+
+            alarmAfter.Text = s;
+
+            if (Minutesbudik == DateTime.Now.Minute)
+            {
+                if (Hoursbudik == DateTime.Now.Hour)
+                {
+                    alarmAfter.Text = ("vstávaj");
+                }
+            }
+        }
+
     }
 }
---
+++ Sleep-well/Sleep-well/Settings.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SleepWell
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Settings : ContentPage
+    {
+        public Settings()
+        {
+            InitializeComponent();
+        }
+
+        void OpenMainPage(object sender, EventArgs args)
+        {
+            App.Current.MainPage = new MainPage();
+        }
+    }
+}
